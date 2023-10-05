98971161c02acec29c01b31fbe840e14f191c212
Fabio <fassil0203@gmail.com>
2023-10-05 05:39:11 -0300

    Adicionando dowhile String

---
+++ src/String/Main.java
@@ -0,0 +1,22 @@
+package String;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String original = "abcde FGHIJ  ABC abc DEFG " ;
+		
+		String s01 =original.toLowerCase();
+		String s02 = original.toUpperCase();
+		String s03 = original.trim();
+		
+		
+		
+		System.out.println("Original : -" + original + "-");
+		System.out.println("toLowerCase: - " + s01 + "-");
+		System.out.println("toUpperCase: -" + s01 + "-");
+		System.out.println("trim:-" + s03+ "-");
+
+	}
+
+}
